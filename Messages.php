20c9285504ee5d191d28b75d7e1b6ba3bdf414b5
BlackBetty15 <milica.ninkovic95@gmail.com>
2017-09-18 21:30:01 +0200

    More methods added to the message class

--- Messages.php
+++ Messages.php
@@ -37,18 +37,190 @@ class Messages
         
         $qry="INSERT INTO posta(naslov ,  posiljalac , posiljalac_ime, posiljalac_prezime,  procitana ,  poruka ,  odgovoreno)
         VALUES ('".$subject."','".$sender."','".$senderFirstName."','".$senderLastName."',0,'".$message."',0)";
+
+        $responseStatus=Connection::queryRequest($qry);
+        if($responseStatus){
+            return 'Uspešno ste poslali poruku';
+        }
+        else{
+            return 'Došlo je do greške, pokušajte kasnije';
+        }
+
     }
 
     public static function listAllMessages(){
 
+        $qry="SELECT * FROM posta WHERE 1";
+        $results=Connection::queryRequest($qry);
+        $response=array();
+
+        if(Connection::emptyQueryResults($results)){
+            while($row=$results->fetch_assoc()){
+                $response[]=$row;
+            }
+        }
+        return $response;
 
     }
-    public static function listAllRespondedMessages(){}
-    public static function getOneMessage($id){}
-    public static function getReadMessages(){}
-    public static function getUnreadMessages(){}
-    public static function respondedMessages(){}
-    public static function deleteMessages(){}
+    public static function listAllRespondedMessages(){
+
+        $qry="SELECT * FROM posta WHERE odgovoreno=1";
+        $results=Connection::queryRequest($qry);
+        $response=array();
+
+        if(Connection::emptyQueryResults($results)){
+            while($row= $results->fetch_assoc()){
+                $response[]=$row;
+            }
+        }
+        return $response;
+    }
+
+    public static function getOneMessage($id){
+
+        $qry="SELECT * FROM posta WHERE id=$id";
+        $result=Connection::queryRequest($qry);
+
+        if($result){
+            return $result;
+        }
+        else{
+            return 'Tražena poruka ne postoji';
+        }
+    }
+
+    public static function readMessage($id){
+        $qry="UPDATE posta SET procitana=1 WHERE  id=$id";
+        $result=Connection::queryRequest($qry);
+
+        if($result){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+
+    public static function markAllRead($ids){
+
+        $queryCondition='WHERE ';
+        $size=sizeof($ids);
+
+        for($i=0;$i<$size;$i++){
+
+            if($i!=$size-1){
+                $queryCondition.="id=".$ids[$i]."OR";
+            }
+            else{
+                $queryCondition.="id=".$ids[$i];
+            }
+        }
+
+        $qry="UPDATE posta SET procitana=1 ".$queryCondition;
+
+        $result=Connection::queryRequest($qry);
+
+        if($result){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static function deleteAllMessages($ids){
+        $queryCondition='WHERE ';
+        $size=sizeof($ids);
+
+        for($i=0;$i<$size;$i++){
+
+            if($i!=$size-1){
+                $queryCondition.="id=".$ids[$i]."OR";
+            }
+            else{
+                $queryCondition.="id=".$ids[$i];
+            }
+        }
+        $qry="DELETE  FROM posta ".$queryCondition;
+        $result=Connection::queryRequest($qry);
+
+        if($result){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static function markAllUnread($ids){
+        $queryCondition='WHERE ';
+        $size=sizeof($ids);
+
+        for($i=0;$i<$size;$i++){
+
+            if($i!=$size-1){
+                $queryCondition.="id=".$ids[$i]."OR";
+            }
+            else{
+                $queryCondition.="id=".$ids[$i];
+            }
+        }
+
+        $qry="UPDATE posta SET procitana=0 ".$queryCondition;
+        $result=Connection::queryRequest($qry);
+
+        if($result){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static function getReadMessages(){
+        $qry="SELECT * FROM posta WHERE procitana=1";
+        $results=Connection::queryRequest($qry);
+
+        $response=array();
+
+        if(Connection::emptyQueryResults($results)){
+            while($row=$results->fetch_assoc()){
+                $results[]=$row;
+            }
+        }
+        return $response;
+
+    }
+    public static function getUnreadMessages(){
+        $qry="SELECT * FROM posta WHERE procitana=0";
+        $results=Connection::queryRequest($qry);
+
+        $response=array();
+
+        if(Connection::emptyQueryResults($results)){
+            while($row=$results->fetch_assoc()){
+                $results[]=$row;
+            }
+        }
+        return $response;
+
+    }
+    public static function respondedMessages(){
+
+        $qry="SELECT * FROM posta WHERE odgovoreno=1";
+        $results=Connection::queryRequest($qry);
+
+        $response=array();
+
+        if(Connection::emptyQueryResults($results)){
+            while($row=$results->fetch_assoc()){
+                $results[]=$row;
+            }
+        }
+        return $response;
+    }
+
 
 //SELECT  id ,  naslov ,  posiljalac ,  procitana ,  poruka ,  odgovoreno ,  tip ,  datum  FROM  posta  WHERE 1
 }
